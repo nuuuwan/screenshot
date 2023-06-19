7aa58d866d3bb71bb5d140e1725519882ee0f7c1
nuuuwan <nuuuwan@gmail.com>
2023-06-19 14:54:50 +0530

    Added get_time_id_hour

--- src/screenshot/config/config_utils.py
+++ src/screenshot/config/config_utils.py
@@ -1,6 +1,11 @@
 from utils import SECONDS_IN, Time, TimeFormat
 
 
+def get_time_id_hour():
+    ut = Time.now().ut
+    return TimeFormat('%Y%m%d.%H0000').stringify(Time(ut))
+
+
 def get_last_date_id_non_weekend():
     ut = Time.now().ut
     day_of_week = TimeFormat('%a').stringify(Time(ut))
@@ -20,8 +25,3 @@ def get_last_month():
     ut = Time.now().ut
     ut = ut - SECONDS_IN.AVG_MONTH
     return TimeFormat('%B').stringify(Time(ut))
-
-
-if __name__ == '__main__':
-    print(get_last_date_id_non_weekend())
-    print(get_last_month())
