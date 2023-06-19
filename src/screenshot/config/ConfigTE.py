7f051fb2bd5a5dc4aafc50cba06d7b3ead1864c4
nuuuwan <nuuuwan@gmail.com>
2023-06-19 14:43:59 +0530

    Emptied

--- src/screenshot/config/ConfigTE.py
+++
@@ -1,7 +0,0 @@
-from screenshot.config.ConfigImage import ConfigImage
-
-
-class ConfigTE(ConfigImage):
-    def __init__(self, id, description, url, frequency):
-        elem_img_id = 'ImageChart'
-        super().__init__(id, description, url, frequency, elem_img_id)
