6d494d5b2163b00b14c5880e4f9e91a8d05bdaf6
nuuuwan <nuuuwan@gmail.com>
2023-06-19 12:39:13 +0530

    FIX process_config BUGs

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -14,21 +14,29 @@ CRON_FREQUENCY = SECONDS_IN.HOUR
 CRON_OVERLAP = 2
 
 
+def process_config_test(config: Config):
+    log.info(f'process_config_test: config={config}')
+
+    config.download_image()
+    os.startfile(config.image_path)
+    log.debug(config.tweet_text)
+
+    Tweet(config.tweet_text).add_image(config.image_path)
+
+
 def process_config(config: Config, twitter: Twitter):
-    log.info(f'Processing config={config.id}, {twitter=}')
+    if not ( twitter is not None and config.should_send_tweet):
+        return 
 
-    if twitter is None or config.should_send_tweet:
-        config.download_image()
-        log.debug(config.tweet_text)
+    log.info(f'process_config: config={config}, {twitter=}')
 
-        if twitter is None:
-            os.startfile(config.image_path)
+    config.download_image()
+    log.debug(config.tweet_text)
 
     tweet = Tweet(config.tweet_text).add_image(config.image_path)
-    if twitter is not None:
-        twitter.send(tweet)
-        log.debug(f'😴 Sleeping for {T_SLEEP_SECONDS}s...')
-        time.sleep(T_SLEEP_SECONDS)
+    twitter.send(tweet)
+    log.debug(f'😴 Sleeping for {T_SLEEP_SECONDS}s...')
+    time.sleep(T_SLEEP_SECONDS)
 
 
 def init_twitter():
@@ -41,12 +49,12 @@ def init_twitter():
 
 def main():
     twitter = init_twitter()
-    config_list = CONFIG_LIST
-    if twitter is None:
-        config_list = config_list[-1:]
 
-    for config in config_list:
-        process_config(config, twitter)
+    if twitter is None:
+        process_config_test(CONFIG_LIST[-1])
+    else:
+        for config in CONFIG_LIST:
+            process_config(config, twitter)
 
 
 if __name__ == '__main__':
