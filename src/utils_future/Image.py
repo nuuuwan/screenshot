bfc60669cb706d909d4db7559c76f036b8f9f53b
nuuuwan <nuuuwan@gmail.com>
2023-06-20 18:28:57 +0530

    log.debug(f'Drew text "{text}" at {lefttop}')

--- src/utils_future/Image.py
+++ src/utils_future/Image.py
@@ -55,5 +55,5 @@ class Image:
     def draw_text(self, lefttop: Point2D, text: str):
         draw = ImageDraw.Draw(self.im)
         draw.text(lefttop.to_tuple(), text, (0, 0, 0), font=FONT)
-        log.debug(f'Drew text ({text}) at {lefttop}')
+        log.debug(f'Drew text "{text}" at {lefttop}')
         return Image(self.im)
