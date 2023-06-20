f956cef73d079d2bdbc83fa2d5ac636df302ec8f
nuuuwan <nuuuwan@gmail.com>
2023-06-20 17:30:11 +0530

    FIX timestamp BUG

--- src/screenshot/config/Ventusky.py
+++ src/screenshot/config/Ventusky.py
@@ -1,4 +1,6 @@
-from utils import TIMEZONE_OFFSET, Time, TimeFormat
+from utils import SECONDS_IN, TIMEZONE_OFFSET, Time, TimeFormat
+
+from screenshot.config.Config import TIME_FORMAT
 
 LAT, LNG = 7.87, 80.65
 ZOOM = 8
@@ -8,12 +10,17 @@ class Ventusky:
     def __init__(self, data_type: str):
         self.data_type = data_type
 
+    @staticmethod
+    def get_time(delta_ut: int):
+        Q = SECONDS_IN.HOUR * 3
+        start_ut = int(Time.now().ut / Q) * Q
+        ut = start_ut + delta_ut
+        return Time(ut)
+
     def get_url_from_time(self, delta_ut: int):
-        ut = Time.now().ut + delta_ut
-        # 20230621/0400
         time_id = TimeFormat(
             '%Y%m%d/%H00', timezone_offset=TIMEZONE_OFFSET.GMT
-        ).stringify(Time(ut))
+        ).stringify(Ventusky.get_time(delta_ut))
         return (
             'https://www.ventusky.com'
             + f'/?p={LAT};{LNG};{ZOOM}'
@@ -22,7 +29,4 @@ class Ventusky:
         )
 
     def get_timestamp(self, delta_ut: int):
-        ut = Time.now().ut + delta_ut
-        return TimeFormat(
-            '%Y-%m-%d %H:00', timezone_offset=TIMEZONE_OFFSET.LK
-        ).stringify(Time(ut))
+        return TIME_FORMAT.stringify(Ventusky.get_time(delta_ut))
