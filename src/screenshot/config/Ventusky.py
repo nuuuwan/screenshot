e6bcc7d759049c87a94da93dea4b22c1ee08c1d2
nuuuwan <nuuuwan@gmail.com>
2023-06-20 10:27:04 +0530

    Added Ventusky

---
+++ src/screenshot/config/Ventusky.py
@@ -0,0 +1,20 @@
+from utils import Time, TimeFormat
+
+LAT, LNG = 7.87, 80.65
+ZOOM = 8
+
+
+class Ventusky:
+    def __init__(self, data_type: str):
+        self.data_type = data_type
+
+    def get_url_from_time(self, delta_ut: int):
+        ut = Time.now().ut + delta_ut
+        # 20230621/0400
+        time_id = TimeFormat('%Y%m%d/%H00').stringify(Time(ut))
+        return (
+            'https://www.ventusky.com'
+            + f'/?p={LAT};{LNG};{ZOOM}'
+            + f'&l={self.data_type}'
+            + f'&t={time_id}'
+        )
