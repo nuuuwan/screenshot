0cf0b5f36972d5e79f7e794acfe1d9f3e1e78fac
nuuuwan <nuuuwan@gmail.com>
2023-06-20 18:05:08 +0530

    Added ConfigVentusky

---
+++ src/screenshot/config/ConfigVentusky.py
@@ -0,0 +1,26 @@
+from utils import SECONDS_IN
+
+from screenshot.config.ConfigScreenshotAnimation import \
+    ConfigScreenshotAnimation
+from screenshot.config.Ventusky import Ventusky
+from utils_future import Point2D, Size2D
+
+
+class ConfigVentusky(ConfigScreenshotAnimation):
+    def __init__(self, title: str, ventusky_id: str):
+        name = f'ventusky.{ventusky_id}'
+        ventusky = Ventusky(ventusky_id)
+        urls = [
+            ventusky.get_url_from_time(SECONDS_IN.HOUR * i)
+            for i in range(0, 24, 3)
+        ]
+        timestamps = [
+            ventusky.get_timestamp(SECONDS_IN.HOUR * i)
+            for i in range(0, 24, 3)
+        ]
+        duration = SECONDS_IN.HOUR * 6
+        lefttop = Point2D(0, 0)
+        size = Size2D(1920, 1920)
+        super().__init__(
+            name, title, urls, timestamps, duration, lefttop, size
+        )
