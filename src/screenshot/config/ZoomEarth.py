7bc29d707f52ad2760cf4881ee878ef9584cccf4
nuuuwan <nuuuwan@gmail.com>
2023-08-03 14:09:01 +0530

    Added ZoomEarth

---
+++ src/screenshot/config/ZoomEarth.py
@@ -0,0 +1,32 @@
+from utils import SECONDS_IN, TIMEZONE_OFFSET, Time, TimeFormat
+
+from screenshot.config.Config import TIME_FORMAT
+
+LAT, LNG = 7.87, 80.65
+ZOOM = 9
+
+
+class ZoomEarth:
+    def __init__(self, data_type: str):
+        self.data_type = data_type
+
+    @staticmethod
+    def get_time(delta_ut: int):
+        Q = SECONDS_IN.HOUR * 3
+        start_ut = int(Time.now().ut / Q) * Q
+        ut = start_ut + delta_ut
+        return Time(ut)
+
+    def get_url_from_time(self, delta_ut: int):
+        time_id = TimeFormat(
+            '%Y-%m-%d,%H:00', timezone_offset=TIMEZONE_OFFSET.GMT
+        ).stringify(ZoomEarth.get_time(delta_ut))
+        return (
+            'https://zoom.earth/maps'
+            + f'/{self.data_type}'
+            + f'/#view={LAT},{LNG},{ZOOM}z'
+            + f'/date={time_id},+5.5/model=icon'
+        )
+
+    def get_timestamp(self, delta_ut: int):
+        return TIME_FORMAT.stringify(ZoomEarth.get_time(delta_ut))
