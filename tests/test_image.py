e7f8690dfc0f9b47ce7d060c5e130518665e48b1
nuuuwan <nuuuwan@gmail.com>
2023-09-10 07:52:10 +0530

    Test resize. Change test image names.

--- tests/test_image.py
+++ tests/test_image.py
@@ -3,11 +3,18 @@ import unittest
 
 from utils_future import Image
 
-TEST_IMAGE1_PATH = os.path.join('tests', 'image1.png')
-TEST_IMAGE2_PATH = os.path.join('tests', 'image2.png')
+TEST_IMAGE_PATH = os.path.join('tests', 'image.original.png')
 
 
 class TestCase(unittest.TestCase):
     def test_image_equalize_hue(self):
-        im = Image.load(TEST_IMAGE1_PATH)
-        im.equalize_hue().write(TEST_IMAGE2_PATH)
+        im = Image.load(TEST_IMAGE_PATH)
+        im.equalize_hue().write(
+            TEST_IMAGE_PATH.replace('original.png', 'equalize_hue.png')
+        )
+
+    def test_image_resize(self):
+        im = Image.load(TEST_IMAGE_PATH)
+        im.resize(ratio=0.5).write(
+            TEST_IMAGE_PATH.replace('original.png', 'resize05.png')
+        )
