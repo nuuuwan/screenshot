75ea8f2b763374cd4bd3ca98bfaa2871beb83de1
nuuuwan <nuuuwan@gmail.com>
2023-09-12 06:47:55 +0530

    Use LocationConfigs; Added LOCATION_CONFIG_COLOMBO

--- workflows/CONFIG_LISTS/VENTUSKY.py
+++ workflows/CONFIG_LISTS/VENTUSKY.py
@@ -1,28 +1,55 @@
-from screenshot import ConfigVentusky
+from screenshot import ConfigVentusky, LocationConfig
+
+
+LOCATION_CONFIG_SRI_LANKA = LocationConfig(
+    'sri_lanka', 7.87, 80.65, 8
+)
+
+LOCATION_CONFIG_COLOMBO = LocationConfig(
+    'colombo', 6.93, 79.86, 10
+)
 
 CONFIG_LIST_VENTUSKY = [
+    # SRI_LANKA
     ConfigVentusky(
         'temperature-2m',
         '#Temperature',
+        LOCATION_CONFIG_SRI_LANKA,
     ),
     ConfigVentusky(
         'temp-feel',
         'Perceived (#FeelsLike) #Temperature',
+        LOCATION_CONFIG_SRI_LANKA,
     ),
     ConfigVentusky(
         'rain-3h',
         '#Rainfall',
+        LOCATION_CONFIG_SRI_LANKA,
     ),
     ConfigVentusky(
         'humidity',
         '#RelativeHumidity (#RH)',
+        LOCATION_CONFIG_SRI_LANKA,
     ),
     ConfigVentusky(
         'gust',
         '#WindGusts',
+        LOCATION_CONFIG_SRI_LANKA,
     ),
     ConfigVentusky(
         'aqi',
         'Air Quality (#AQI)',
+        LOCATION_CONFIG_SRI_LANKA,
+    ),
+    # COLOMBO
+    ConfigVentusky(
+        'rain-3h',
+        '#Rainfall',
+        LOCATION_CONFIG_COLOMBO,
+    ),
+    ConfigVentusky(
+        'aqi',
+        'Air Quality (#AQI)',
+        LOCATION_CONFIG_COLOMBO,
     ),
 ]
