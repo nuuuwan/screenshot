fe552291ac3cb8efe22016dc4f5fa0067838bdf2
nuuuwan <nuuuwan@gmail.com>
2023-09-16 16:04:51 +0530

    Added more image tests

--- tests/test_image.py
+++ tests/test_image.py
@@ -3,18 +3,23 @@ import unittest
 
 from utils_future import Image
 
-TEST_IMAGE_PATH = os.path.join('tests', 'image.original.png')
+IMAGE_ID_LIST = ['image', 'image2']
+TEST_IMAGE_PATH_LIST = [
+    os.path.join('tests', f'{id}.original.png') for id in IMAGE_ID_LIST
+]
 
 
 class TestCase(unittest.TestCase):
     def test_image_equalize_value(self):
-        im = Image.load(TEST_IMAGE_PATH)
-        im.equalize_value().write(
-            TEST_IMAGE_PATH.replace('original.png', 'equalize_value.png')
-        )
+        for test_image_path in TEST_IMAGE_PATH_LIST:
+            im = Image.load(test_image_path)
+            im.equalize_value().write(
+                test_image_path.replace('original.png', 'equalize_value.png')
+            )
 
     def test_image_resize(self):
-        im = Image.load(TEST_IMAGE_PATH)
-        im.resize(ratio=0.5).write(
-            TEST_IMAGE_PATH.replace('original.png', 'resize05.png')
-        )
+        for test_image_path in TEST_IMAGE_PATH_LIST:
+            im = Image.load(test_image_path)
+            im.resize(ratio=0.5).write(
+                test_image_path.replace('original.png', 'resize05.png')
+            )
