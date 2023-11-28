3f27ca42d6ffaac53e52b07ea391289bf8cfa75f
nuuuwan <nuuuwan@gmail.com>
2023-11-28 10:15:39 +0530

    optimized ceb

--- workflows/configs/ceb.py
+++ workflows/configs/ceb.py
@@ -5,43 +5,28 @@ from screenshot import ConfigScreenshot
 from utils_future import Point2D, Size2D16x9
 
 
+def get_config(id: str, label: str) -> ConfigScreenshot:
+    return ConfigScreenshot(
+        f'ceb.power_generation.{id}',
+        f'{label} by @CEB_lk',
+        'https://cebcare.ceb.lk/gensum/details',
+        SECONDS_IN.DAY,
+        Point2D(0, 0),
+        Size2D16x9(1120),
+        (By.ID, id),
+    )
+
+
 def get_config_list():
     return [
-        ConfigScreenshot(
-            'ceb.power_generation.load_curve',
-            'Load Curve by @CEB_lk',
-            'https://cebcare.ceb.lk/gensum/details',
-            SECONDS_IN.DAY,
-            Point2D(0, 0),
-            Size2D16x9(1120),
-            (By.ID, 'graph_load_curve'),
+        get_config('graph_load_curve', 'Load Curve'),
+        get_config('pie_energy_data', 'Daily Energy Share'),
+        get_config(
+            'table_reservoir_rainfall_data_wrapper',
+            'Present Level and Rainfall at ' + 'Major Reservoirs',
         ),
-        ConfigScreenshot(
-            'ceb.power_generation.daily_energy_share',
-            'Daily Energy Share by @CEB_lk',
-            'https://cebcare.ceb.lk/gensum/details',
-            SECONDS_IN.DAY,
-            Point2D(0, 0),
-            Size2D16x9(1120),
-            (By.ID, 'pie_energy_data'),
-        ),
-        ConfigScreenshot(
-            'ceb.power_generation.major_reservoirs',
-            'Present Level and Rainfall at ' + 'Major Reservoirs by @CEB_lk',
-            'https://cebcare.ceb.lk/gensum/details',
-            SECONDS_IN.DAY,
-            Point2D(0, 0),
-            Size2D16x9(1120),
-            (By.ID, 'table_reservoir_rainfall_data_wrapper'),
-        ),
-        ConfigScreenshot(
-            'ceb.power_generation.small_reservoirs',
-            'Present Level and Rainfall at '
-            + 'Smaller Reservoirs or Ponds by @CEB_lk',
-            'https://cebcare.ceb.lk/gensum/details',
-            SECONDS_IN.DAY,
-            Point2D(0, 0),
-            Size2D16x9(1120),
-            (By.ID, 'table_pond_rainfall_data_wrapper'),
+        get_config(
+            'table_pond_rainfall_data_wrapper',
+            'Present Level and Rainfall at ' + 'Smaller Reservoirs or Ponds',
         ),
     ]
