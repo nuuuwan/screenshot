c5fb244265fdcb942c065a8d7ff58c44ae016c45
nuuuwan <nuuuwan@gmail.com>
2023-11-28 10:00:22 +0530

    Split globalpetrolprices

---
+++ workflows/configs/globalpetrolprices.py
@@ -0,0 +1,23 @@
+from utils import SECONDS_IN
+
+from screenshot import ConfigScreenshot
+from utils_future import Point2D, Size2D
+
+
+def get_config(id: str, label: str) -> ConfigScreenshot:
+    return ConfigScreenshot(
+        f'globalpetrolprices.{id}',
+        f'{label} by @GlobalPetrol',
+        f'https://www.globalpetrolprices.com/{id}/',
+        SECONDS_IN.DAY * 2,
+        Point2D(0, 400),
+        Size2D(800, 3400),
+    )
+
+
+CONFIG_LIST = [
+    get_config('gasoline_prices', 'Global #Gasoline (#Petrol) Prices'),
+    get_config('diesel_prices', 'Global #Diesel Prices'),
+    get_config('lpg_prices', 'Global #LPG (#Gas) Prices'),
+    get_config('electricity_prices', 'Global #Electricity Prices'),
+]
