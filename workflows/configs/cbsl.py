689b793ed9f68ee963418d9238bc81a37e0b5fec
nuuuwan <nuuuwan@gmail.com>
2023-12-08 12:40:32 +0530

    cbsl.macroeconomic_charts 2023- Q3

--- workflows/configs/cbsl.py
+++ workflows/configs/cbsl.py
@@ -51,11 +51,8 @@ def get_config_list():
         ),
         ConfigScreenshot(
             'cbsl.macroeconomic_charts',
-            'Macroeconomic Charts (2023 Q1) by @CBSL',
-            'https://www.cbsl.gov.lk'
-            + '/sites/default/files'
-            + '/cbslweb_documents/statistics/mecpac'
-            + '/Chart_Pack_Q1_2023_e1.pdf#page='
+            'Macroeconomic Charts (2023 Q3) by @CBSL',
+            'https://www.cbsl.gov.lk/sites/default/files/cbslweb_documents'+'/statistics/mecpac/Chart_Pack_Q3_2023_e.pdf#page='
             + str(random.randint(4, 46)),
             SECONDS_IN.DAY,
             Point2D(1920 - 1557, 1920 - 1886),
--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -18,7 +18,7 @@ PROD_LOG_PATH = os.path.join(DIR_TEMP, 'prod.log')
 
 # Should be consistent with pipeline-cron.yml
 CRON_FREQUENCY = SECONDS_IN.MINUTE * 20
-TEST_CONFIG_ID_PART = 'aqicn.aq.lk'
+TEST_CONFIG_ID_PART = 'cbsl.macroeconomic_charts'
 
 
 def init_dir():
