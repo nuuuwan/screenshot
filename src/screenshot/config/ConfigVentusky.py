12081d0725c66764d0d02ec09e3f404d74cbaf91
nuuuwan <nuuuwan@gmail.com>
2023-12-12 09:07:19 +0530

    duration = SECONDS_IN.DAY * 2

--- src/screenshot/config/ConfigVentusky.py
+++ src/screenshot/config/ConfigVentusky.py
@@ -26,7 +26,7 @@ class ConfigVentusky(ConfigScreenshotAnimation):
             ventusky.get_timestamp(SECONDS_IN.HOUR * i)
             for i in range(0, 24, 3)
         ]
-        duration = SECONDS_IN.DAY
+        duration = SECONDS_IN.DAY * 2
         lefttop = Point2D(0, 0)
         size = Size2D(1920, 1920)
         super().__init__(
