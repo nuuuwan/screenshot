f0511e92c3368b3ebbf70634ad5083b3abcac775
Nuwan I. Senaratna <nuuuwan@gmail.com>
2024-01-10 09:36:56 +0530

    Update nuuuwan.py

--- workflows/configs/nuuuwan.py
+++ workflows/configs/nuuuwan.py
@@ -11,7 +11,7 @@ def get_config_list():
             'lanka_data_search',
             '#LankaDataSearch',
             'https://nuuuwan.github.io/lanka_data_search/',
-            SECONDS_IN.HOUR * 3,
+            SECONDS_IN.HOUR * 2,
             Point2D(0, 0),
             Size2D16x9(640),
             (By.ID, 'multi-line-chart'),
