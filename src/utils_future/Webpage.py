4253f9567425ac8ffae70650c3f4cba42a2db6d5
nuuuwan <nuuuwan@gmail.com>
2024-05-15 14:58:37 +0530

    news_lk_bulletin: updated dims

--- src/utils_future/Webpage.py
+++ src/utils_future/Webpage.py
@@ -30,7 +30,6 @@ class Webpage:
             ['ourworldindata.org', [960, 960]],
             ['globalpetrolprices', [800, 4200]],
             ['www.google.com/maps', [1200, 675]],
-            ['https://github.com/nuuuwan/news_lk_bulletin', [700, 2100]],
             ['weather_lk', [3200, 3200]],
         ]:
             if url_str in url:
