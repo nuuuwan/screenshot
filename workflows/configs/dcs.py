ec510e21acb0bf370caa1763b0626f26c173a2d7
nuuuwan <nuuuwan@gmail.com>
2025-02-10 11:00:09 +0530

    https://www.statistics.gov.lk/Publication/PocketBook2024

--- workflows/configs/dcs.py
+++ workflows/configs/dcs.py
@@ -9,18 +9,18 @@ from utils_future import Point2D, Size2D, Size2D16x9
 def get_config_list():
     return [
         ConfigScreenshot(
-            'dcs_snapshot.chart',
-            'Economic Snapshot by @LankaSTAT #DCS',
-            'http://www.statistics.gov.lk',
+            "dcs_snapshot.chart",
+            "Economic Snapshot by @LankaSTAT #DCS",
+            "http://www.statistics.gov.lk",
             TimeUnit.SECONDS_IN.WEEK,
             Point2D(1060, 160),
             Size2D(460, 340),
         ),
         ConfigScreenshot(
-            'statistics.sri_lanka',
-            'Statistical Pocket Book 2023\nvia @LankaSTAT',
-            'http://www.statistics.gov.lk/Publication/PocketBook2023#'
-            + 'page=%d' % (random.randint(13, 103)),
+            "statistics.sri_lanka",
+            "Statistical Pocket Book 2024\nvia @LankaSTAT",
+            "https://www.statistics.gov.lk/Publication/PocketBook2024#"
+            + "page=%d" % (random.randint(13, 103)),
             TimeUnit.SECONDS_IN.HOUR * 12,
             Point2D(470, 60),
             Size2D16x9(980),
