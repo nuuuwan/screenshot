a84b075fea07c1b2abd0ac9b7cb496e1d12926a4
nuuuwan <nuuuwan@gmail.com>
2025-06-24 11:26:14 +0530

    Added us_state_gov.tgs_map

--- workflows/configs/_misc.py
+++ workflows/configs/_misc.py
@@ -10,31 +10,27 @@ last_month = config_utils.get_last_month()
 def get_config_list():
     return [
         ConfigScreenshot(
-            'lpw.property_price.chart',
-            'Average #Property Prices via @LankaProperty',
-            'https://www.lankapropertyweb.com/house_prices.php',
+            "lpw.property_price.chart",
+            "Average #Property Prices via @LankaProperty",
+            "https://www.lankapropertyweb.com/house_prices.php",
             TimeUnit.SECONDS_IN.WEEK,
             Point2D(15, 120),
             Size2D(950, 700),
         ),
         ConfigScreenshot(
-            'prisons_lk.statistics',
-            'Statistics by Dept. of #Prisons',
-            'http://prisons.gov.lk/web/en/statistics-information-en/',
+            "prisons_lk.statistics",
+            "Statistics by Dept. of #Prisons",
+            "http://prisons.gov.lk/web/en/statistics-information-en/",
             TimeUnit.SECONDS_IN.WEEK,
             Point2D(360, 360),
             Size2D16x9(840),
         ),
+        ConfigScreenshot(
+            "us_state_gov.tgs_map",
+            "#USA @TravelGov | #Travel Advisories Map",
+            "https://travelmaps.state.gov/TSGMap//",
+            TimeUnit.SECONDS_IN.DAY,
+            Point2D(490, 540),
+            Size2D16x9(1400),
+        ),
     ]
-
-
-'''
-    ConfigScreenshot(
-        '',
-        '',
-        '',
-        TimeUnit.SECONDS_IN.DAY,
-        Point2D(1920- 1920, 1920- 1920),
-        Size2D(1920, 1920),
-    ),
-'''
